0c17319f602525d16bc6bba2ce96e00489bd2a65
syaofox <syaofox@gmail.com>
2022-03-30 11:55:46 +0800

    update

--- scripts/dm-desktop.sh
+++ scripts/dm-desktop.sh
@@ -81,10 +81,9 @@ installsuckless(){
 configsuckless() {
 	clear
 
-	sudo pacman -S xorg xorg-xinit
-	pressanykey
-	tip "sudo pacman -S picom lxappearance qt5ct nautilus ark feh jq fzf baobab xautolock xfce4-power-manager xfce4-screenshooter eog webp-pixbuf-loader gnome-calculator parcellite numlockx dunst libnotify udisks2 udiskie gnome-keyring libsecret libgnome-keyring seahorse polkit-gnome"
-	sudo pacman -S picom lxappearance qt5ct nautilus ark feh jq zfz baobab xautolock xfce4-power-manager xfce4-screenshooter eog webp-pixbuf-loader gnome-calculator parcellite numlockx dunst libnotify udisks2 udiskie gnome-keyring libsecret libgnome-keyring seahorse polkit-gnome
+
+	tip "sudo pacman -S xorg xorg-xinit picom lxappearance qt5ct nautilus ark feh jq fzf baobab xautolock xfce4-power-manager xfce4-screenshooter eog webp-pixbuf-loader gnome-calculator parcellite numlockx dunst libnotify udisks2 udiskie gnome-keyring libsecret libgnome-keyring seahorse polkit-gnome"
+	sudo pacman -S xorg xorg-xinit picom lxappearance qt5ct nautilus ark feh jq fzf baobab xautolock xfce4-power-manager xfce4-screenshooter eog webp-pixbuf-loader gnome-calculator parcellite numlockx dunst libnotify udisks2 udiskie gnome-keyring libsecret libgnome-keyring seahorse polkit-gnome
 	pressanykey
 	paru -S  ly
 
