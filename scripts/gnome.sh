2e0561a29a2c1ca58ad8db647043f2b95a1a4842
syaofox <syaofox@gmail.com>
2022-03-30 20:15:49 +0800

    update

--- scripts/gnome.sh
+++ scripts/gnome.sh
@@ -7,7 +7,7 @@ installgnomemenu() {
     sudo pacman -S --noconfirm xorg gdm gnome gnome-extra firefox gnome-tweaks gnome-software-packagekit-plugin flatpak
     sudo systemctl enable gdm
     tip "paru -S chrome-gnome-shell  gnome-terminal-transparency"
-    paru -S --noconfirm chrome-gnome-shell  gnome-terminal-transparency gnome-software-packagekit-plugin
+    paru -S chrome-gnome-shell  gnome-terminal-transparency gnome-software-packagekit-plugin
 
     pressanykey
 }
