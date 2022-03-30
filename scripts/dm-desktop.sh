db3e20a24fb22077e4dbe62df59e4e1b19a08de6
syaofox <syaofox@gmail.com>
2022-03-30 11:39:20 +0800

    update

--- scripts/dm-desktop.sh
+++ scripts/dm-desktop.sh
@@ -81,14 +81,14 @@ installsuckless(){
 configsuckless() {
 	clear
 
-	sudo pacman -S --noconfirm xorg xorg-xinit picom lxappearance qt5ct  nautilus  ark feh jq zfz baobab xautolock xfce4-power-manager xfce4-screenshooter eog webp-pixbuf-loader gnome-calculator parcellite numlockx dunst libnotify udisks2 udiskie gnome-keyring libsecret libgnome-keyring seahorse polkit-gnome
+	# sudo pacman -S --noconfirm xorg xorg-xinit picom lxappearance qt5ct  nautilus  ark feh jq zfz baobab xautolock xfce4-power-manager xfce4-screenshooter eog webp-pixbuf-loader gnome-calculator parcellite numlockx dunst libnotify udisks2 udiskie gnome-keyring libsecret libgnome-keyring seahorse polkit-gnome
   
 
 
-	paru -S --noconfirm ly
+	# paru -S --noconfirm ly
 
-    sudo systemctl enable ly
-	sudo systemctl enable udisks2
+    # sudo systemctl enable ly
+	# sudo systemctl enable udisks2
 	
 	tip "Modify .xinitrc"
     cp /etc/X11/xinit/xinitrc ~/.xinitrc
@@ -100,8 +100,8 @@ configsuckless() {
 
     cat  "exec dwm" >> ~/.xinitrc 
 
-	overwrightdotfiles common
-	overwrightdotfiles dwm  
+	# overwrightdotfiles common
+	# overwrightdotfiles dwm  
 	
 	
 	pressanykey
